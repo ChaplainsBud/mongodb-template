6ee4003ed1d0b60d43ca4dc6866f702cde0ca834
Matt Leroe <matthew.robert.leroe@gmail.com>
2021-11-04 19:02:02 -0400

    UI to Express, to MongoDB

--- public/ui2.js
+++ public/ui2.js
@@ -1,4 +1,4 @@
-let showcase = document.querySelector('h1');
+let showcase = document.querySelector('p');
 showcase.style.color = "purple";
 
 // showcase.innerHTML = `How do I change this to the Posted value?`;
--- server.js
+++ server.js
@@ -2,6 +2,38 @@ import { MongoClient } from 'mongodb';
 import express from 'express';
 const app = express();
 
+// Connection URL
+const url = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ignqx.mongodb.net/Sandbox?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'Sandbox';
+
+async function main(expressProvidedVariable) {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('kittens');
+
+  // the following code examples can be pasted here...
+  
+  //CREATE
+  // const insertResult = await collection.insertMany([{ name: "Timmy" }, { name: "Titus" }, { name: "Fred" }]);
+  // console.log('Inserted documents =>', insertResult);
+
+
+// I'm moving this above 
+  const insertResult = await collection.insertOne({name: `${expressProvidedVariable}`});
+  console.log('Inserted documents =>', insertResult);
+
+  
+  return 'done.';
+}
+
+
+
+
 app.get('/')
 app.listen(3000)
 
@@ -30,74 +62,16 @@ app.get('/hmm', (req, res) => {
   console.log(req.query)
   // can I put a MongoDB driver CRUD function here? 
   
-
-})
-
-// res.render('hmm', { variable for pug? })
-
-// req.body -> can be put in a POST method, raw/test:json; try Postman
-// req.hostname -> can be taken by a GET method, this and all below can be put in an object, and console.dir(obj); 
-// req.params
-// req.query
-// req.param
-
-// res.json() -> respond with a {"key":"value"}
-// res.render('index')
-// res.send('hello express') 
-
-
-
-
-///////////////////// DB below; Express Above
-
-console.log('working so far')
-
-// Connection URL
-const url = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ignqx.mongodb.net/Sandbox?retryWrites=true&w=majority';
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'Sandbox';
-
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-
-
-  // the following code examples can be pasted here...
-  
-  //CREATE
-  // const insertResult = await collection.insertMany([{ name: "Timmy" }, { name: "Titus" }, { name: "Fred" }]);
-  // console.log('Inserted documents =>', insertResult);
-
-
-// I'm moving this above 
-  // const insertResult = await collection.insertOne({name: `${globvar}`});
-  // console.log('Inserted documents =>', insertResult);
-
-  //READ
-  // const findResult = await collection.find({}).toArray();
-  // console.log('Found documents =>', findResult);
-
-  // const filteredDocs = await collection.find({ a: 3 }).toArray();
-  // console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
-
-  //UPDATE
-  // const updateResult = await collection.updateOne({ a: 3 }, { $set: { b: 1 } });
-  // console.log('Updated documents =>', updateResult);
-
-  //DELETE
-  // const deleteResult = await collection.deleteMany({ a: 3 });
-  // console.log('Deleted documents =>', deleteResult);
-
-  return 'done.';
-}
-
-main()
+  main(globvar)
   .then(console.log)
   .catch(console.error)
   // what if I keep this open, and add mongo driver CRUD function to Express function? 
   // .finally(() => client.close());
+
+})
+
+
+
+
 
   
